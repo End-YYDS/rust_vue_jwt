30ef1398ba7b0e923aa76c24f78f42541dec43a7
derrick921213 <dlin12457@gmail.com>
2024-05-04 15:20:38 +0800

    fix

--- prepare.ps1
+++ prepare.ps1
@@ -4,10 +4,12 @@ function Ensure-CommandExists {
         [string]$command,
         [string]$installScript
     )
+    $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
     $exists = $null -ne (Get-Command $command -ErrorAction SilentlyContinue)
     if (-not $exists) {
         Write-Host "$command is not installed. Attempting to install..."
         Invoke-Expression $installScript
+        $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
         $exists = $null -ne (Get-Command $command -ErrorAction SilentlyContinue)
         if (-not $exists) {
             Write-Host "Failed to install $command. Please install it manually and try again."
@@ -22,16 +24,16 @@ $gitInstallScript = {
     $asset = Invoke-RestMethod -Method Get -Uri $git_url | % assets | where name -like "*64-bit.exe"
     $installer = "$env:temp\$($asset.name)"
     Invoke-WebRequest -Uri $asset.browser_download_url -OutFile $installer
-    $git_install_inf = "$projectPath/Config/git.cfg"
-    $install_args = "/SP- /VERYSILENT /SUPPRESSMSGBOXES /NOCANCEL /NORESTART /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS /LOADINF=""$git_install_inf"""
-    Start-Process -FilePath $installer -ArgumentList $install_args -Wait
+    $git_install_inf = "$projectPath\Config\git.cfg"
+    $install_args = "/SP- /VERYSILENT /SUPPRESSMSGBOXES /NOCANCEL /NORESTART /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS /LOADINF=$($git_install_inf)"
+    Start-Process -FilePath $installer -ArgumentList "$install_args" -Wait
 }
 
 $npmInstallScript = {
     $asset = "node-v20.12.2-x64.msi"
     $installer = "$env:temp\$($asset)"
     Invoke-WebRequest -Uri "https://nodejs.org/dist/v20.12.2/node-v20.12.2-x64.msi" -OutFile $installer
-    Start-Process "msiexec.exe" -ArgumentList "/i $installer /quiet" -Wait
+    Start-Process powershell -verb runas -ArgumentList "msiexec.exe /i $installer /quit" -Wait
 }
 
 $rustInstallScript = {
@@ -39,12 +41,13 @@ $rustInstallScript = {
     $installer = "$env:temp\$($asset)"
     Invoke-WebRequest -Uri "https://static.rust-lang.org/rustup/dist/i686-pc-windows-gnu/rustup-init.exe" -OutFile $installer
     $install_args = "-y"
-    Start-Process -FilePath $installer -ArgumentList $install_args -Wait
+    Start-Process -FilePath $installer -ArgumentList "$install_args" -Wait
 }
 
 Ensure-CommandExists -command "git" -installScript $gitInstallScript
 Ensure-CommandExists -command "npm" -installScript $npmInstallScript
 Ensure-CommandExists -command "cargo" -installScript $rustInstallScript
+
 
 Set-Location -Path $projectPath/frontend
 
